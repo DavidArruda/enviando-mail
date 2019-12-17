c48f415adec728321f1788a5e98f7f388716756d
DavidArruda <deh0_@hotmail.com>
2019-12-17 14:53:12 -0200

    Criação teste junit Para enviar email smtp

--- src/test/java/enviando/mail/AppTest.java
+++ src/test/java/enviando/mail/AppTest.java
@@ -1,38 +1,60 @@
 package enviando.mail;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	private String userName = "davidtestejava@gmail.com";
+	private String password = "@Lenovo2020";
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testeEmail() {
+
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true"); // Autorização
+			properties.put("mail.smtp.starttls", "true"); // Autenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com"); // Servidor E-MAIL
+			properties.put("mail.smtp.port", "465"); // Porta do servidor
+			properties.put("mail.smtp.socketFactory.port", "465"); // Expecifica a porta a ser conectado pelo socket
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Classe socket de conexao ao SMTP
+
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+
+			Address[] toUser = InternetAddress.parse("deh0_@hotmail.com, davidarruda762@gmail.com");
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName)); // Quem está enviando
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("Chegou o E-MAIL enviado com Java"); // Assunto do Email
+			message.setText("Email do Java");
+
+			Transport.send(message);
+
+			System.out.println(session);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
